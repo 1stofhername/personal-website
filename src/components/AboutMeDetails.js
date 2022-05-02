fba3c9045a3fb70b76e39670ed7824363a4710b0
Alicia <arrowsrp@yahoo.com>
2022-05-01 20:23:10 -0700

    Adds job history.

--- src/components/AboutMeDetails.js
+++ src/components/AboutMeDetails.js
@@ -1,11 +1,11 @@
 export default function AboutMeDetails () {
     return (
-        <div className="details">
-            <h2 className="sub-title">ABOUT ME</h2>
-            <p className='paragraph'>
-                <span className="bold-text">HELLO!</span> I'm a <span className="bold-text">WEB DEVELOPER</span> actively seeking a position with opportunity for <span id="job-attribute" className="job-attributes">GROWTH,</span> <span className="job-attributes" id="job-attribute-2">CHALLENGES, </span>and <span className="job-attributes" id="job-attribute-3">INNOVATION</span>.
+        <div className="about-paragraph-container">
+            <h2 id ='about-me-sub-title' className="sub-title">ABOUT ME</h2>
+            <p className='about-paragraph'>
+                <span className="bold-text">HELLO!</span> I'm a <span className="bold-text">WEB DEVELOPER</span> actively seeking a position with opportunity for <span id="job-attribute" className="bold-text">GROWTH,</span> <span className="bold-text" id="job-attribute-2">CHALLENGES, </span>and <span className="bold-text" id="job-attribute-3">INNOVATION</span>.
             </p>
-            <p  className='paragraph'>
+            <p  className='about-paragraph'>
                 I  enjoy building  <span className="bold-text">CREATIVE SOLUTIONS</span> to crack <span className="bold-text">COMPLEX PROBLEMS</span> and bringing my <span className="bold-text">UNIQUE PERSPECTIVE</span> to any teams I am a part of.
             </p>
         </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,78 +3,26 @@ import React, { useEffect, useState } from 'react';
 
 import AboutMeDetails from './AboutMeDetails';
 import SuperPowers from './SuperPowers';
-
-
-import me from '../assets/me-pic.png';
-import location from '../assets/icons/location.svg'
-import linkedin from '../assets/icons/linkedin.svg'
-import envelope from '../assets/icons/envelope.svg'
-import plane from '../assets/icons/plane.svg'
-import github from '../assets/icons/github.svg'
+import PersonalInfo from './PersonalInfo';
 
 
 export default function Landing () {
 
-    function toggleLocationText () {
-        const relocate = document.getElementById('relocate');
-        const seattle = document.getElementById('seattle');
-        const location = document.getElementById('location-icon');
-        const plane = document.getElementById('plane-icon');
-
-        relocate.removeAttribute('hidden'); seattle.setAttribute('hidden', true);
-        plane.removeAttribute('hidden'); location.setAttribute('hidden', true);
-        setTimeout(()=>{
-            relocate.setAttribute("hidden", true); seattle.removeAttribute('hidden');
-            plane.setAttribute("hidden", true); location.removeAttribute('hidden');
-        },3000)
-    };
+    
 
     return (
         <div className='landing'>
-        <div className='column-one'>
-            <div className="header">
-
-                <div className="title">
-                    <h1 className="name">Alicia Parris</h1>
-                    <h2 className="sub-title">Fullstack Developer</h2>
-                </div>
-
-                <img id="me" src={me} alt="photo of me"/>
-
-                <div className="contact-info-container">
-                    <span id="email">
-                        <a href="mailto:aliciaparris@zoho.com">
-                            <img className="contact-icons" src={envelope}/>
-                        </a>
-                    </span>
-                    <span id="linkedin">
-                        <a href="http://www.linkedin.com/in/idealcandidate" target="blank">
-                            <img className="contact-icons" src={linkedin}/>
-                        </a>
-                    </span>
-                    <span id="github">
-                        <a href="https://github.com/1stofhername" target="blank">
-                            <img className="contact-icons" src={github}/>
-                        </a>
-                    </span>
-                    <span id="location-info">
-                        <img id="location-icon" onMouseOver={()=>{toggleLocationText()}} className="contact-icons" src={location}/>
-                        <img id="plane-icon" className="contact-icons" src={plane} hidden/>
-
-                        <p id="relocate" className='location-text' hidden>Willing to relocate</p>
-                        <p id="seattle" className='location-text' onMouseOver={()=>{toggleLocationText()}}>SEATTLE, WA</p>
-                    </span>
-                </div>
+            <div className='column-one'>
+                <PersonalInfo />
+            </div>
+            <div className="column-two">
+                <AboutMeDetails />
+                <SuperPowers />
             </div>
             <div className="arrow">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-        <div className="column-two">
-            <AboutMeDetails />
-            <SuperPowers />
+                    <span></span>
+                    <span></span>
+                    <span></span>
             </div>
         </div>
     )
